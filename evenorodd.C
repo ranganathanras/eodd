bfd62b48aa2865fa4b00b4842dcb1f859a106a7f
ranganathanras <30952596+ranganathanras@users.noreply.github.com>
2017-08-17 16:42:14 +0530

    even or odd
    to find given number is even or odd

---
+++ evenorodd.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("\n Enter a number");
+scanf("%d",&a);
+if(a%2==0)
+printf("\n Entered number is even");
+else
+printf("\n Entered number is odd");
+getch();
+}
